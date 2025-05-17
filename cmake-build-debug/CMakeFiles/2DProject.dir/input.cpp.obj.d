7c6cb4883ab6be4714b250b76914d254291e3c84
Shawn Bohn <shawnb456324@gmail.com>
2025-05-16 22:54:44 -0400

    Added in features for GObject.cpp and Texture.cpp and Sheets so you can make animation. Also added in Transform.cpp so that objects can have some relative space. Next patch to add physics of some sort

---
+++ cmake-build-debug/CMakeFiles/2DProject.dir/input.cpp.obj.d
@@ -0,0 +1,50 @@
+CMakeFiles/2DProject.dir/input.cpp.obj: \
+ C:\Users\shawn\CLionProjects\2DProject\input.cpp \
+ C:\Users\shawn\CLionProjects\2DProject\input.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_events.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_stdinc.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_config.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_platform.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/begin_code.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/close_code.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/winsdkver.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/winapifamily.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/sdkddkver.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stddef.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/stddef.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stdarg.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/stdarg.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_stdarg.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stdint.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/stdint.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_error.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_video.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_pixels.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_endian.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_rect.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_rwops.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_surface.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_blendmode.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_keyboard.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_keycode.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_scancode.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_mouse.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_joystick.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_guid.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_mutex.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_gamecontroller.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_sensor.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_quit.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_gesture.h \
+ C:/Users/shawn/CLionProjects/2DProject/SDL/include/SDL2/SDL_touch.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/process.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/corecrt_startup.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/sys/types.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h
