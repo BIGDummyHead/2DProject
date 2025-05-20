55718017b8ca8f522c5e99f48c2f9ce6c98f578b
Shawn Bohn <shawnb456324@gmail.com>
2025-05-19 22:37:53 -0400

    started workin on camera/render

---
+++ cmake-build-debug/CMakeFiles/2DProject.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "2DProject.exe"
+  "2DProject.exe.manifest"
+  "2DProject.pdb"
+  "CMakeFiles/2DProject.dir/Game/Background.cpp.obj"
+  "CMakeFiles/2DProject.dir/Game/Background.cpp.obj.d"
+  "CMakeFiles/2DProject.dir/Game/Camera.cpp.obj"
+  "CMakeFiles/2DProject.dir/Game/Camera.cpp.obj.d"
+  "CMakeFiles/2DProject.dir/Game/Collider.cpp.obj"
+  "CMakeFiles/2DProject.dir/Game/Collider.cpp.obj.d"
+  "CMakeFiles/2DProject.dir/Game/GObject.cpp.obj"
+  "CMakeFiles/2DProject.dir/Game/GObject.cpp.obj.d"
+  "CMakeFiles/2DProject.dir/Game/Sheet.cpp.obj"
+  "CMakeFiles/2DProject.dir/Game/Sheet.cpp.obj.d"
+  "CMakeFiles/2DProject.dir/Game/Texture.cpp.obj"
+  "CMakeFiles/2DProject.dir/Game/Texture.cpp.obj.d"
+  "CMakeFiles/2DProject.dir/Game/Transform.cpp.obj"
+  "CMakeFiles/2DProject.dir/Game/Transform.cpp.obj.d"
+  "CMakeFiles/2DProject.dir/Vector2.cpp.obj"
+  "CMakeFiles/2DProject.dir/Vector2.cpp.obj.d"
+  "CMakeFiles/2DProject.dir/draw.cpp.obj"
+  "CMakeFiles/2DProject.dir/draw.cpp.obj.d"
+  "CMakeFiles/2DProject.dir/init.cpp.obj"
+  "CMakeFiles/2DProject.dir/init.cpp.obj.d"
+  "CMakeFiles/2DProject.dir/input.cpp.obj"
+  "CMakeFiles/2DProject.dir/input.cpp.obj.d"
+  "CMakeFiles/2DProject.dir/main.cpp.obj"
+  "CMakeFiles/2DProject.dir/main.cpp.obj.d"
+  "lib2DProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2DProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
